55ba914edeb9f7bab8f1e527ac1b03948cea1074
GarryFr <garry_f@mail.ru>
2018-04-17 16:53:10 +0300

    Update Data.java

--- src/main/java/com/rest/domain/Data.java
+++ src/main/java/com/rest/domain/Data.java
@@ -24,32 +24,4 @@ public class Data {
 		return data;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((data == null) ? 0 : data.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Data other = (Data) obj;
-		if (data == null) {
-			if (other.data != null)
-				return false;
-		} else if (!data.equals(other.data))
-			return false;
-		return true;
-	}
-
-	
-	 
-
 }
